e9a8014f9fba9edbb087476bf3cbda54e3559f5e
Joe Kaufeld <joe.kaufeld@gmail.com>
2018-09-22 01:06:07 -0400

    fix import install issue

--- charlotte/__init__.py
+++ charlotte/__init__.py
@@ -4,8 +4,6 @@ from charlotte.config import config
 from charlotte.engine import Base as Prototype
 from charlotte.errors import *
 
-__version__ = "0.2.0"
-
 __all__ = [
     "Prototype",
     "CharlotteConfigurationError",
